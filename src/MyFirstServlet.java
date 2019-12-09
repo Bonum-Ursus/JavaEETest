0e69ea40f852214eee009c6438ac3b74f8d0d60a
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2019-12-09 13:00:16 +0500

    Initial commit

---
+++ src/MyFirstServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1>My FIRST servlet</h1>");
+        pw.println("</html>");
+    }
+}
---
+++ web/firstJSP.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 09.12.2019
+  Time: 12:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>First JSP</title>
+</head>
+<body>
+    <h1>Testing JSP</h1>
+    <p>
+        <%
+            java.util.Date now = new java.util.Date();
+            String someString = "Текущая дата: " + now;
+        %>
+        <%= someString%>
+        <%
+            for (int i = 0; i < 10; i++) {
+                out.println("<p>" + "Count: " + i + "</p>");
+            }
+        %>
+    </p>
+</body>
+</html>
